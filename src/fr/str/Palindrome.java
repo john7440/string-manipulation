d43bec744a679755768bd8c1cd968144ada965ff
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-29 10:43:17 +0100

    ajout d'une normalisation pour enlever les accents du text avant vérification palindrome

--- src/fr/str/Palindrome.java
+++ src/fr/str/Palindrome.java
@@ -1,18 +1,24 @@
 package fr.str;
 import java.util.Scanner;
+import java.text.Normalizer;
 
 public class Palindrome {
 	
 	// Question 5.4
 	public static boolean isPalindrome(String text){
 		
+		// First we normalize the text by decomposing letters then remove accents
+		String normalizedText = Normalizer.normalize(text, Normalizer.Form.NFD)
+											.replaceAll("\\p{M}", "");
+		
 		// We clean the original text, remove spaces and put everything into lower case:
-		String cleanedText = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+		String cleanedText = normalizedText.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 		
 		// We reverse the cleaned text
 		String reversedText = new StringBuilder(cleanedText).reverse().toString();
 		
-		// We compare both text, if they are the same, then the text is a palindrome:
+		// We compare both text, if they are the same, return true if they are,
+		// return false otherwise:
 		return cleanedText.equals(reversedText);
 	}
 
@@ -24,6 +30,7 @@ public class Palindrome {
 		String textToCheck = scan.nextLine();
 		
 		boolean result = isPalindrome(textToCheck);
+		
 		if (result) {
 			System.out.println(textToCheck + " is a palindrome!");
 		} else {
